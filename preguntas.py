dcb37b6d21b86c1c7a41b29c585f05726675f3d7
adcalderonc <112212138+adcalderonc@users.noreply.github.com>
2022-12-02 00:46:50 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -18,8 +18,8 @@ def pregunta_01():
     df = pd.read_csv('gm_2008_region.csv')
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = df['life']
-    X = df['fertility']
+    y = df['life'].values
+    X = df['fertility'].values
 
     # Imprima las dimensiones de `y`
     print(y.shape)
@@ -28,10 +28,10 @@ def pregunta_01():
     print(X.shape)
 
     # Transforme `y` a un array de numpy usando reshape
-    y_reshaped = y.values.reshape(y.shape[0], 1)
+    y_reshaped = y.reshape(-1, 1)
 
     # Trasforme `X` a un array de numpy usando reshape
-    X_reshaped = X.values.reshape(X.shape[0], 1)
+    X_reshaped = X.reshape(-1, 1)
 
     # Imprima las nuevas dimensiones de `y`
     print(y_reshaped.shape)
@@ -53,16 +53,16 @@ def pregunta_02():
     print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print("{:.4f}".format(df['life'].corr(df['fertility'])))
+    print(round(df['life'].corr(df['fertility']), 4))
     
     # Imprima la media de la columna `life` con 4 decimales.
-    print("{:.4f}".format(df['life'].mean()))
+    print(round(df['life'].mean(), 4))
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(df['fertility'].__class__)
+    print(type(df['fertility']))
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print("{:.4f}".format(df['GDP'].corr(df['life'])))
+    print(round(df['GDP'].corr(df['life']), 4))
     
 
 def pregunta_03():
@@ -75,10 +75,10 @@ def pregunta_03():
     df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df["fertility"]
+    X_fertility = df["fertility"].values.reshape(-1, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = df["life"]
+    y_life = df["life"].values.reshape(-1, 1)
     
     # Importe LinearRegression
     from sklearn.linear_model import LinearRegression
@@ -94,13 +94,13 @@ def pregunta_03():
     ).reshape(-1, 1)
 
     # Entrene el modelo usando X_fertility y y_life
-    reg.fit(X_fertility.values.reshape(-1, 1), y_life.values.reshape(-1, 1))
+    reg.fit(X_fertility, y_life)
 
     # Compute las predicciones para el espacio de predicción
     y_pred = reg.predict(prediction_space)
 
     # Imprima el R^2 del modelo con 4 decimales
-    print(reg.score(X_fertility.values.reshape(-1, 1), y_life.values.reshape(-1, 1)).round(4))
+    print(reg.score(X_fertility, y_life).round(4))
 
 
 def pregunta_04():
@@ -120,16 +120,17 @@ def pregunta_04():
     df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df["fertility"].values
-    
+    X_fertility = df["fertility"].values.reshape(-1, 1)
+
     # Asigne a la variable los valores de la columna `life`
-    y_life = df["life"].values
+    y_life = df["life"].values.reshape(-1, 1)
+
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
     (X_train, X_test, y_train, y_test,) = train_test_split(
-        (X_fertility,
-         y_life,
+        X_fertility,
+        y_life,
         test_size=0.20,
         random_state=53,)
 
@@ -137,12 +138,12 @@ def pregunta_04():
     linearRegression = LinearRegression()
 
     # Entrene el clasificador usando X_train y y_train
-     linearRegression.fit(X_train.reshape(-1, 1), y_train.reshape(-1, 1))
+    linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
-    y_pred = linearRegression.predict(X_test.reshape(-1, 1))
+    y_pred = linearRegression.predict(X_test)
         
     # Compute and print R^2 and RMSE
-    print("R^2: {:6.4f}".format(linearRegression.score(X_test.reshape(-1, 1), y_test.reshape(-1, 1))))
+    print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
     rmse = mean_squared_error(y_test, y_pred, squared = False)
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
